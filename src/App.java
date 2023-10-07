4e6d7a80aa25b48b61b8aefd4564fc8349103939
bgprudhomme <bprudhomme@gradcenter.cuny.edu>
2023-10-07 18:33:03 -0400

    Some renaming

--- src/App.java
+++ src/App.java
@@ -33,16 +33,16 @@ public class App {
         for(int i=0; i<numTerms; i++)
             terms.add(in.nextInt());
         
-        System.out.println(hhMax(terms));
-
+        // System.out.println(maxHH(terms));
+        // System.out.println(minHH(terms));
     }
 
-    public static AdjMatrix hhMax(ArrayList<Integer> terms){
+    public static AdjMatrix maxHH(ArrayList<Integer> terms){
         AdjMatrix result = new AdjMatrix(terms.size());
         
         ArrayList<TreeSet<Integer>> bins = new ArrayList<TreeSet<Integer>>();
         
-        int k, curDeg, startNode, endNode;
+        int k, curDeg, pivotNode, neighborNode;
         HashMap<Integer, Integer> movedNodes;
 
         for(int i=0; i<terms.size(); i++){
@@ -66,30 +66,96 @@ public class App {
             while(bins.get(k).isEmpty())
                 k--;
 
-            startNode = bins.get(k).pollFirst();
-            bins.get(0).add(startNode);
+            pivotNode = bins.get(k).pollFirst();
+            bins.get(0).add(pivotNode);
             curDeg = k;
 
-            System.out.println("Pivot node: " + startNode);
+            System.out.println("Pivot node: " + pivotNode);
             System.out.println(bins);
 
-            // endNode = 0;
+            // neighborNode = 0;
 
             for(int i=0; i<k; i++){
-                // while(bins.get(0).contains(endNode))
-                //     endNode++;
+                // while(bins.get(0).contains(neighborNode))
+                //     neighborNode++;
                 while((bins.get(curDeg).isEmpty() || (movedNodes.keySet().contains(bins.get(curDeg).first()) && bins.get(curDeg).size() == 1)) && curDeg > 0){
                     curDeg--;
                 }
                 if(curDeg == 0)
                     return null;
                 else {
-                    endNode = bins.get(curDeg).pollFirst();
-                    movedNodes.put(endNode, curDeg-1);
-                    // bins.get(curDeg-1).add(endNode);
-                    result.addEdge(startNode, endNode);
-                    result.addEdge(endNode, startNode);
-                    System.out.println("Added edge: (" + startNode + ", " + endNode + ")");
+                    neighborNode = bins.get(curDeg).pollFirst();
+                    movedNodes.put(neighborNode, curDeg-1);
+                    // bins.get(curDeg-1).add(neighborNode);
+                    result.addEdge(pivotNode, neighborNode);
+                    result.addEdge(neighborNode, pivotNode);
+                    System.out.println("Added edge: (" + pivotNode + ", " + neighborNode + ")");
+                    System.out.println(bins);
+                }
+            }
+
+            for(int node : movedNodes.keySet())
+                bins.get(movedNodes.get(node)).add(node);
+            System.out.println("Moved nodes");
+            System.out.println(bins);
+
+        }
+        return result;
+    }
+
+    public static AdjMatrix minHH(ArrayList<Integer> terms){
+        AdjMatrix result = new AdjMatrix(terms.size());
+        
+        ArrayList<TreeSet<Integer>> bins = new ArrayList<TreeSet<Integer>>();
+        
+        int k, curDeg, pivotNode, neighborNode;
+        HashMap<Integer, Integer> movedNodes;
+
+        for(int i=0; i<terms.size(); i++){
+            bins.add(new TreeSet<Integer>());
+        }
+        for(int i=0; i<terms.size(); i++){
+            bins.get(terms.get(i)).add(i);
+        }
+
+        System.out.println(bins);
+
+        int rounds = 0;
+
+        while(bins.get(0).size() != terms.size()){
+
+            System.out.println("Round " + ++rounds);
+
+            movedNodes = new HashMap<Integer, Integer>();
+            
+            k = 1;
+            while(bins.get(k).isEmpty())
+                k++;
+
+            pivotNode = bins.get(k).pollFirst();
+            bins.get(0).add(pivotNode);
+            curDeg = terms.size()-1;
+
+            System.out.println("Pivot node: " + pivotNode);
+            System.out.println(bins);
+
+            // neighborNode = 0;
+
+            for(int i=0; i<k; i++){
+                // while(bins.get(0).contains(neighborNode))
+                //     neighborNode++;
+                while((bins.get(curDeg).isEmpty() || (movedNodes.keySet().contains(bins.get(curDeg).first()) && bins.get(curDeg).size() == 1)) && curDeg > 0){
+                    curDeg--;
+                }
+                if(curDeg == 0)
+                    return null;
+                else {
+                    neighborNode = bins.get(curDeg).pollFirst();
+                    movedNodes.put(neighborNode, curDeg-1);
+                    // bins.get(curDeg-1).add(neighborNode);
+                    result.addEdge(pivotNode, neighborNode);
+                    result.addEdge(neighborNode, pivotNode);
+                    System.out.println("Added edge: (" + pivotNode + ", " + neighborNode + ")");
                     System.out.println(bins);
                 }
             }
